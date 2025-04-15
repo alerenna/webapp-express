c0a46087e447b979fc6bd63011082432747466d8
“alerenna” <alessandrorennafm@gmail.com>
2025-04-15 19:59:08 +0800

    Add database configuration files and update .gitignore and package.json

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3005
+const connection = require('./data/db')
 
 
 app.listen(port, () => {
@@ -9,4 +10,6 @@ app.listen(port, () => {
 
 app.get('/', (req, res) => {
     console.log(`That's the movie server`);
+    res.send('MOvie API server!')
 })
+
